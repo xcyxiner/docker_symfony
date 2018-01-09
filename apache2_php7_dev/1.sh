241709f785217166d8800b11620286a76daf2f2f
xcyxiner <593091948@qq.com>
2018-01-09 08:58:18 +0000

    apache2_php7_dev doctrine/mongodb-odm php7 不稳定，还是换回php5.6

--- apache2_php7_dev/1.sh
+++ apache2_php7_dev/1.sh
@@ -1,2 +1,2 @@
 docker build -t xcyxiner/symfony:dev .
-docker run -p 7777:80 --link docker_mongo_1:mongo --rm  -v /opt/Uniwork/Source/test:/var/www/html -t -i xcyxiner/symfony:dev /bin/bash
+#docker run -p 7777:80 --link docker_mongo_1:mongo --rm  -v /opt/Uniwork/Source/test:/var/www/html -t -i xcyxiner/symfony:dev /bin/bash
